58a4ad091f98adfd32cbcc065413d0c686659c9f
Abakash Rudra <aborudra@gmail.com>
2025-08-14 18:28:41 +0530

    first version of the app

--- serverSide/src/server.mjs
+++ serverSide/src/server.mjs
@@ -6,10 +6,13 @@ import chatRouter from './routes/chat.router.mjs'
 import { connectDB } from './db_Connect/db.mjs'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
+
 
 dotenv.config()
 const app=express()
 const PORT=process.env.PORT || 5050
+const __dirname = path.resolve()
 
 app.use(cors({
     origin: 'http://localhost:5173', // React app URL
@@ -25,6 +28,14 @@ app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use('/api/user',userRouter)
 app.use('/api/chat',chatRouter)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../clientSide/dist')))
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../clientSide','dist', 'index.html'))
+    })
+}
 
 app.get('/',(request,response)=>{
     response.send('hellow world')
