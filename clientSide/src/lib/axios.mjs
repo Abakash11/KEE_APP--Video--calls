e636a1c43b4c600f03edfb3a52b03078eae4e42c
Abakash Rudra <132146503+Abakash11@users.noreply.github.com>
2025-08-14 21:18:57 +0530

    Update axios.mjs

--- clientSide/src/lib/axios.mjs
+++ clientSide/src/lib/axios.mjs
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const baseURL=import.meta.env.MODE === 'development' ? 'http://localhost:5050/api' : '/api';
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: baseURL,
   withCredentials: true,
 });
 
